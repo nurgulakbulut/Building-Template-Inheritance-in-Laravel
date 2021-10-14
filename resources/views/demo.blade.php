6c9926853ffbcd1185cf94bb5826c366b856a996
nurgulakbulut <nurgulakbulut.95@gmail.com>
2021-10-14 11:12:31 +0000

    Building Template inheritance

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title','Calisma')
+
+@section('content')
+     <h1>'Hello World!!!' </h1>
+     <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Temporibus ad saepe sed inventore aperiam tempore, doloremque enim dicta dignissimos eos!
+          A neque asperiores minus illum ipsa error quas corporis aut.
+     </p>
+@endsection
